0403aa76f621afb6b74601ffd5fab39a99ad146a
Jason Felice <jason.m.felice@gmail.com>
2025-12-08 16:04:24 -0500

    Separate UI

--- src/net/eraserhead/arbor/webui.cljs
+++ src/net/eraserhead/arbor/webui.cljs
@@ -1,13 +1,13 @@
-(ns net.eraserhead.arbor
+(ns net.eraserhead.arbor.webui
   (:require
    [reagent.dom.client :as rdc]
    [re-frame.core :as rf]))
 
-(defonce root (rdc/create-root (js/document.getElementById "app")))
-
 (defn- arbor []
   [:ul
    [:li "hello"]])
+
+(defonce root (rdc/create-root (js/document.getElementById "app")))
 
 (defn ^:dev/after-load start []
   (rdc/render root [arbor]))
