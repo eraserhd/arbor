5ddd474480cbafbbbf9649d9e1bd0308d2f841e8
Jason Felice <jason.m.felice@gmail.com>
2025-12-09 14:45:24 -0500

    Install ADK

--- flake.nix
+++ flake.nix
@@ -7,12 +7,26 @@
   outputs = { self, nixpkgs, flake-utils }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.android_sdk.accept_license = true;
+        };
+
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          platformVersions = [ "34" ];
+          buildToolsVersions = [ "34.0.0" ];
+          includeEmulator = false;
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             clojure
+            androidComposition.androidsdk
           ];
+
+          ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+          ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
         };
     }));
 }
