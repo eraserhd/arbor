e55583ff39010978a3be4d52b43b0f0b40be965b
Jason Felice <jason.m.felice@gmail.com>
2025-12-12 18:21:20 -0500

    Persist app-db in localStorage

--- src/net/eraserhead/arbor/webui/events.cljs
+++ src/net/eraserhead/arbor/webui/events.cljs
@@ -1,12 +1,32 @@
 (ns net.eraserhead.arbor.webui.events
   (:require
+   [cljs.reader]
    [net.eraserhead.arbor :as arbor]
    [net.eraserhead.arbor.loci :as loci]
    [re-frame.core :as rf]))
 
+(defn- try-read-string [s]
+  (try
+   (cljs.reader/read-string s)
+   (catch js/Error e
+     nil)))
+
 (rf/reg-event-db
  ::initialize
- (constantly arbor/initial-state))
+ (fn [_ _]
+   (or (some-> js/localStorage
+               (.getItem "db")
+               try-read-string)
+       arbor/initial-state)))
+
+;; Persist app-db to localStorage all the time
+(rf/reg-global-interceptor
+  (rf/->interceptor
+    :id :persist
+    :after (fn [context]
+             (when-let [db (rf/get-effect context :db)]
+               (.setItem js/localStorage "db" (pr-str db)))
+             context)))
 
 (rf/reg-event-db
  ::new-machine
