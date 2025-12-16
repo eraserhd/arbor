af2591f0a8722d95465c56716aad499a5949c49d
Jason Felice <jason.m.felice@gmail.com>
2025-12-16 14:24:17 -0500

    Adjust

--- test/net/eraserhead/arbor/bluetooth_test.clj
+++ test/net/eraserhead/arbor/bluetooth_test.clj
@@ -1,22 +1,23 @@
 (ns net.eraserhead.arbor.bluetooth-test
   (:require
-   [clojure.test :refer [deftest is]]
+   [clojure.test :refer [deftest testing is]]
    [net.eraserhead.arbor.bluetooth :as bt]))
 
 (deftest t-device-list-arrived
-  (let [devices (-> (bt/device-list-arrived {:db {}} [::bt/device-list-arrived
-                                                      [{::bt/id "00:00:01"
-                                                        ::bt/name "Foo"
-                                                        ::bt/address "00:00:01"}
-                                                       {::bt/id "00:00:02"
-                                                        ::bt/name "Bar"
-                                                        ::bt/address "00:00:02"}]])
-                    (get-in [:db ::bt/devices]))]
-    (is (= {::bt/id "00:00:01"
-            ::bt/name "Foo"
-            ::bt/address "00:00:01"}
-           (get devices "00:00:01")))
-    (is (= {::bt/id "00:00:02"
-            ::bt/name "Bar"
-            ::bt/address "00:00:02"}
-           (get devices "00:00:02")))))
+  (testing "new devices found"
+    (let [devices (-> (bt/device-list-arrived {:db {}} [::bt/device-list-arrived
+                                                        [{::bt/id "00:00:01"
+                                                          ::bt/name "Foo"
+                                                          ::bt/address "00:00:01"}
+                                                         {::bt/id "00:00:02"
+                                                          ::bt/name "Bar"
+                                                          ::bt/address "00:00:02"}]])
+                      (get-in [:db ::bt/devices]))]
+      (is (= {::bt/id "00:00:01"
+              ::bt/name "Foo"
+              ::bt/address "00:00:01"}
+             (get devices "00:00:01")))
+      (is (= {::bt/id "00:00:02"
+              ::bt/name "Bar"
+              ::bt/address "00:00:02"}
+             (get devices "00:00:02"))))))
